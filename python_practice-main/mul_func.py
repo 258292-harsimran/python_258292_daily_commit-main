286f41cbe650f0e5939c0d7ad3503800438561c2
258292-harsimran <harsimranbatra1999@gmail.com>
2021-04-19 13:06:55 +0530

    practicing function def

---
+++ python_practice-main/mul_func.py
@@ -0,0 +1,12 @@
+def mul(num3,num4):
+    print(num3,num4)
+    num5=num3*num4
+    print(num5)
+    
+    
+
+print("multiplication of ")
+num3 = 5
+num4 = 5
+
+print(mul(num3,num4))
--- python_practice-main/prac.py
+++ python_practice-main/prac.py
@@ -1,6 +1,10 @@
-print('258292-HARSIMRAN_SINGH')
+#print('258292-HARSIMRAN_SINGH')
 
-num1=5
-num2=5
-sum=num1+num2
-print(sum)
+#num1=5
+# num2=5
+# sum=num1+num2
+# print(sum)
+
+
+
+
