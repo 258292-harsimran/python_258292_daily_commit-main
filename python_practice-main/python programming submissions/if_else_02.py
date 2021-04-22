0bd6e01bafe6714f811941eedb5c945b8f4bfe49
258292-harsimran <harsimranbatra1999@gmail.com>
2021-04-22 22:42:49 +0530

    if_else_02

---
+++ python_practice-main/python programming submissions/if_else_02.py
@@ -0,0 +1,12 @@
+print("Enter your abbreviation")
+string = input()
+if string == "lol":
+    print("laughing out loud")
+elif string == "rofl":
+    print("rolling on the floor laughingd")
+elif string == "lmk":
+    print("let me know")
+elif string == "smh":
+    print( "shaking my head")
+else:
+    print("System dont know this abbreviation")
